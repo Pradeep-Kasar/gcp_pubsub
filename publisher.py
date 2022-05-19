0db4700583c994f21def1f954106dd2da4ba9430
Pradeep-Kasar <pradeep.kasar@gmail.com>
2022-05-19 16:14:56 -0400

    Add files via upload

---
+++ publisher.py
@@ -0,0 +1,20 @@
+import os 
+from google.cloud import pubsub_v1
+
+credentials_path = '/Users/Pradeep/VisualStudioCode/PubSub/mypubsubproject0709.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = 'projects/mypubsubproject0709/topics/myfirstpubsub'
+
+data = 'set up pubsub using google client libaries and run the python code using VS Code'
+data = data.encode('utf-8')
+
+attributes = {
+'country': 'USA',
+'state': 'Colorado',
+'city': 'Denver'
+}
+
+publish_data = publisher.publish(topic_path, data, **attributes)
+print(f'published message id {publish_data.result()}')
---
+++ subscriber.py
@@ -0,0 +1,33 @@
+import os 
+from google.cloud import pubsub_v1
+from concurrent.futures import TimeoutError
+
+credentials_path = '/Users/Pradeep/VisualStudioCode/PubSub/mypubsubproject0709.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
+
+subscriber = pubsub_v1.SubscriberClient()
+subscriber_path = 'projects/mypubsubproject0709/subscriptions/myfirstpubsub-sub'
+
+def callback(message):
+    print(f'Received message: {message}')
+    print(f'data: {message.data}')
+    
+
+    #if message.attributes():
+    #     print("Attributes:")
+    #     for key in message.attributes:
+    #         value = message.attributes.get(key)
+    #         print(f"{key} : {value}")
+
+    message.ack()
+
+
+subscriber_pull_data = subscriber.subscribe(subscriber_path, callback=callback)
+print(f'Listening to message on {subscriber_path}')
+
+with subscriber:
+    try:
+        subscriber_pull_data.result()
+    except TimeoutError:
+        subscriber_pull_data.cancel()
+        subscriber_pull_data.result()
