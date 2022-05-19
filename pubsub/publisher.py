adeee4928ace498a6198caa6269ad8480b634f7f
Pradeep-Kasar <pradeep.kasar@gmail.com>
2022-05-19 17:49:13 -0400

    Update publisher.py

--- pubsub/publisher.py
+++ pubsub/publisher.py
@@ -7,7 +7,7 @@ os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
 publisher = pubsub_v1.PublisherClient()
 topic_path = 'projects/mypubsubproject0709/topics/myfirstpubsub'
 
-data = 'set up pubsub using google client libaries and run the python code using VS Code'
+data = 'set up pubsub using google client libaries and run the python code using Visual Studio Code'
 data = data.encode('utf-8')
 
 attributes = {
